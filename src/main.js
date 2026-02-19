5502fdd58673eb78c1e6d6f39ad032ee49ca6c15
Fermin Arevalo <minfer29@hotmail.com>
2026-02-19 15:09:48 +0100

    monaco editor

--- src/main.js
+++ src/main.js
@@ -1,62 +1,90 @@
-import './style.css' 
-import Split from 'split-grid'
-import {encode, decode} from 'js-base64'
-import loader from '@monaco-editor/loader'
+import "./style.css";
+import Split from "split-grid";
+import { encode, decode } from "js-base64";
+import * as monaco from "monaco-editor";
+import Htmlworker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import Cssworker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import Jsworker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
+window.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === "html") return new Htmlworker();
+    if (label === "css") return new Cssworker();
+    if (label === "javascript") return new Jsworker();
+  },
+};
 Split({
-	columnGutters: [{
-    track: 1,
-    element: document.querySelector('.vertical-gutter'),
-  }],
-  rowGutters: [{
-  	track: 1,
-    element: document.querySelector('.horizontal-gutter'),
-  }]
-})
+  columnGutters: [
+    {
+      track: 1,
+      element: document.querySelector(".vertical-gutter"),
+    },
+  ],
+  rowGutters: [
+    {
+      track: 1,
+      element: document.querySelector(".horizontal-gutter"),
+    },
+  ],
+});
 
-const $ = selector => document.querySelector(selector)
+const $ = (selector) => document.querySelector(selector);
 
-const $js = $('#js')
-const $css = $('#css')
-const $html = $('#html')
+const $js = $("#js");
+const $css = $("#css");
+const $html = $("#html");
 
-$js.addEventListener('input', update)
-$css.addEventListener('input', update)
-$html.addEventListener('input', update)
+const { pathname } = window.location;
 
-function init (){
-    const { pathname } = window.location
-    
-    const[ rawHtml, rawCss, rawJs] = pathname.slice(1).split('%7C')
+const [rawHtml, rawCss, rawJs] = pathname.slice(1).split("%7C");
 
-    const html= decode(rawHtml)
-    const css= decode(rawCss)
-    const js= decode(rawJs)
-    $html.value = html
-    $css.value = css
-    $js.value = js
+const html = decode(rawHtml);
+const css = decode(rawCss);
+const js = decode(rawJs);
 
-    const htmlForPreview = createHtml ({html,js,css}) 
-    $('iframe').setAttribute('srcdoc', htmlForPreview)
-} 
+const htmlEditor = monaco.editor.create($html, {
+  value: html,
+  language: "html",
+  fontSize: 18,
+  theme: "vs-dark",
+});
 
-function update (){
-    const html = $html.value
-    const css = $css.value
-    const js = $js.value
+const cssEditor = monaco.editor.create($css, {
+  value: css,
+  language: "css",
+  fontSize: 18,
+  theme: "vs-dark",
+});
 
-    const hashedCode = `${encode(html)}|${encode(css)}|${encode(js)}`
+const jsEditor = monaco.editor.create($js, {
+  value: js,
+  language: "javascript",
+  fontSize: 18,
+  theme: "vs-dark",
+});
 
-    window.history.replaceState(null, null, `/${hashedCode}`)
+htmlEditor.onDidChangeModelContent(update);
+cssEditor.onDidChangeModelContent(update);
+jsEditor.onDidChangeModelContent(update);
 
-    const htmlForPreview = createHtml ({html,js,css}) 
-    $('iframe').setAttribute('srcdoc', htmlForPreview)
+const htmlForPreview = createHtml({ html, js, css });
+$("iframe").setAttribute("srcdoc", htmlForPreview);
 
+function update() {
+  const html = htmlEditor.getValue();
+  const css = cssEditor.getValue();
+  const js = jsEditor.getValue();
+
+  const hashedCode = `${encode(html)}|${encode(css)}|${encode(js)}`;
+
+  window.history.replaceState(null, null, `/${hashedCode}`);
+
+  const htmlForPreview = createHtml({ html, js, css });
+  $("iframe").setAttribute("srcdoc", htmlForPreview);
 }
 
-const createHtml = ({html,js,css}) => {
-   
-    return `
+function createHtml({ html, js, css }) {
+  return `
     <!doctype html>
     <html lang="en">
      <head>
@@ -71,6 +99,5 @@ const createHtml = ({html,js,css}) => {
     </script>
     </body>
     </html>
-    `
+    `;
 }
-init()
